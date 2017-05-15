fccfbcc10deca414b14cbe66cab333d5404df0e8
Raphael Gal <raphaelgal@gmail.com>
2017-05-15 12:46:47 -0400

    removed generated comments

--- src/RemoteDatabase/RemoteDatabase.java
+++ src/RemoteDatabase/RemoteDatabase.java
@@ -3,9 +3,6 @@ package RemoteDatabase;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * Created by Raphael on 5/12/2017.
- */
 public interface RemoteDatabase extends Remote {
     public int read() throws RemoteException;
     public void write(int value) throws RemoteException;
